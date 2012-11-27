dfc5a7fe4db43bdc962d0c3c23e559893bfcfee3
Matthew Crossley <vbaspcppguy@gmail.com>
2012-11-27 02:49:47 -0500

    Fuck Postgre.

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ var express = require('express');
 var app = express();
 
 app.get('/hello.txt', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+	var body = 'Hello World';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Length', body.length);
+	res.end(body);
 });
 
 app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
+	res.send('Hello World');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 app.listen(port);
 console.log('Listening on port '+port);
+
+
+
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+
+
+//mysql://:@/heroku_db?reconnect=true
