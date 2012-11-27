94d36e472001742aa9655cb333acb42a75c49c3e
Matthew Crossley <vbaspcppguy@gmail.com>
2012-11-27 03:11:09 -0500

    Playing with Xeround

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello.txt', function(req, res){
-	var body = 'Hello World';
-	res.setHeader('Content-Type', 'text/plain');
-	res.setHeader('Content-Length', body.length);
-	res.end(body);
+app.get('/hello.txt', function(req, res) {
+
+	var start_time = new Date();
+
+	connection.query('SELECT * FROM user', function(err, rows, fields) {
+		if (err) throw err;
+		
+		var body = 'Hello World';
+
+		res.write('Main list: ' + (new Date().getTime() - start_time.getTime()) + '\n');
+		res.write(JSON.stringify(rows));
+		res.end();
+	});
+	
 });
 
-app.get('/hello.txt', function(req, res){
-	res.send('Hello World');
-});
 
 var port = process.env.PORT || 80;
 app.listen(port);
@@ -24,15 +30,3 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
-
-
-//mysql://:@/heroku_db?reconnect=true
