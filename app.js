cc5f1c355d4f3b450ea6d4e9309a19e17666625d
Matthew Crossley <vbaspcppguy@gmail.com>
2012-11-27 03:36:25 -0500

    Response time shits

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var app = express();
 
+var prev = [];
+
 app.get('/hello.txt', function(req, res) {
 
-	var start_time = new Date();
+	var query_start_time = new Date().getTime();
 
 	connection.query('SELECT * FROM user', function(err, rows, fields) {
 		if (err) throw err;
 		
-		var body = 'Hello World';
-
-		res.write('Main list: ' + (new Date().getTime() - start_time.getTime()) + '\n');
-		res.write(JSON.stringify(rows));
+		var delay = new Date().getTime() - query_start_time;
+		
+		res.write('Response Time: ' + delay + 'ms\n');
+		res.write('Data: ' + JSON.stringify(rows) + '\n\n');
+		res.write('Array of previous response times: ' + JSON.stringify(prev) + '\n\n');
 		res.end();
+		
+		prev.push(delay+'ms');
 	});
 	
 });
